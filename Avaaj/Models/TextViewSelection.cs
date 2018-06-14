768acdbd07a2bd84ae63911c109064894b2d9bc5
VMWAREM\vpienkov <vpienkov@vmware.com>
2018-06-13 20:32:08 -0400

    Added text selection and active document

---
+++ Avaaj/Models/TextViewSelection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avaaj
+{
+    public struct TextViewSelection
+    {
+        public int StartPosition { get; set; }
+        public int EndPosition { get; set; }
+        public string Text { get; set; }
+
+        public TextViewSelection(int a, int b, string text)
+        {
+            StartPosition = a;
+            EndPosition = b;
+            Text = text;
+        }
+    }
+}
